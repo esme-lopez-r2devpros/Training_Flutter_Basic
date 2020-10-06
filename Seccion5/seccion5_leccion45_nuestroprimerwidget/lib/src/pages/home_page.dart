dcc11f21ea96a5909f3834dd20cf09e32707998d
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-05 19:39:49 -0500

    Seccion 5 Leccion 48 Columnas y estilos

--- Seccion5/seccion5_leccion45_nuestroprimerwidget/lib/src/pages/home_page.dart
+++ Seccion5/seccion5_leccion45_nuestroprimerwidget/lib/src/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
 
+final estiloTexto = new TextStyle(fontSize: 25);
+
 @override
   Widget build(BuildContext context) {
     
@@ -12,7 +14,13 @@ class HomePage extends StatelessWidget{
         elevation: 2.5,
         ),
         body: Center(
-          child: Text('Hola mundo'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Text('Numero de clicks:', style:estiloTexto),
+              Text('0', style:estiloTexto),
+            ],
+          )
         ),
     );
   }
