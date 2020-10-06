d6450fd84e7ead19cdc977fc004a58f38462c5d5
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-05 20:07:09 -0500

    Seccion 5 Leccion 49 Boton Flotante- FloatingActionButton

--- Seccion5/seccion5_leccion45_nuestroprimerwidget/lib/src/pages/home_page.dart
+++ Seccion5/seccion5_leccion45_nuestroprimerwidget/lib/src/pages/home_page.dart
@@ -14,6 +14,7 @@ final estiloTexto = new TextStyle(fontSize: 25);
         elevation: 2.5,
         ),
         body: Center(
+          
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget> [
@@ -21,6 +22,14 @@ final estiloTexto = new TextStyle(fontSize: 25);
               Text('0', style:estiloTexto),
             ],
           )
+        ),
+        floatingActionButton: FloatingActionButton(
+         child: Icon(Icons.add),
+          //child: Text('Hola'),
+          onPressed: () {
+            print('Hola mundo!');
+          },
+         // onPressed: null, //Es como teener un disable true
         ),
     );
   }
