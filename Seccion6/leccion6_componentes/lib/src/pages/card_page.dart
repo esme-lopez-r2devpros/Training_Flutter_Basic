0bed03489b8106f90e6b4ee6973c9cd87da55fce
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-07 17:24:14 -0500

    Seccion 6 Leccion 73 Image y FadeIn Image

--- Seccion6/leccion6_componentes/lib/src/pages/card_page.dart
+++ Seccion6/leccion6_componentes/lib/src/pages/card_page.dart
@@ -13,32 +13,57 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: [
-          _cardTipo1()
-                  ],
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+                            ],
+                    
+                          ),
+                        );
+                      }
           
+          
+                    
+                     Widget _cardTipo1() {
+          return Card(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.photo_album, color:Colors.blue),
+                  title: Text('Soy el título de esta tarjeta'),
+                  subtitle: Text('Aquí estamos en la descripción de la tarjeta que quiero que ustedes vean que quiero mostrarles. Hoy es Miércoles'),
                 ),
-              );
-            }
-
-
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+          FlatButton(onPressed: () {}, child: Text('Cancelar')),
+          FlatButton(onPressed: () {}, child: Text('Ok')),
+                ],)
+              ],
+            ),
+          );
+                      }
           
-           Widget _cardTipo1() {
+           Widget _cardTipo2() {
 return Card(
-  child: Column(
-    children: [
-      ListTile(
-        leading: Icon(Icons.photo_album, color:Colors.blue),
-        title: Text('Soy el título de esta tarjeta'),
-        subtitle: Text('Aquí estamos en la descripción de la tarjeta que quiero que ustedes vean que quiero mostrarles. Hoy es Miércoles'),
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-FlatButton(onPressed: () {}, child: Text('Cancelar')),
-FlatButton(onPressed: () {}, child: Text('Ok')),
-      ],)
-    ],
-  ),
+child: Column(
+  children: [
+    FadeInImage(
+      placeholder: AssetImage('assets/jar-loading.gif'),
+     image:  NetworkImage('https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'),
+     fadeInDuration: Duration(milliseconds: 200),
+     height: 300.0,
+     fit: BoxFit.cover,
+    ),
+    //Image(
+      //image: NetworkImage('https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'),
+    //),
+    Container(
+      padding: EdgeInsets.all(10.0),
+      child: Text('Imagen tomada en 2021')
+      )
+  ],
+),
 );
             }
 }
