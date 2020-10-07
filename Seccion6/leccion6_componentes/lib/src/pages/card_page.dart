76d580e21675acd031bd0f24d5ba55db5641547d
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-07 18:09:25 -0500

    Seccion 6 Leccion 75 Personalizando el estilo de las Cards

--- Seccion6/leccion6_componentes/lib/src/pages/card_page.dart
+++ Seccion6/leccion6_componentes/lib/src/pages/card_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
-
 class CardPage extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,54 +13,123 @@ class CardPage extends StatelessWidget {
           _cardTipo1(),
           SizedBox(height: 30.0),
           _cardTipo2(),
-                            ],
-                    
-                          ),
-                        );
-                      }
-          
-          
-                    
-                     Widget _cardTipo1() {
-          return Card(
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.photo_album, color:Colors.blue),
-                  title: Text('Soy el título de esta tarjeta'),
-                  subtitle: Text('Aquí estamos en la descripción de la tarjeta que quiero que ustedes vean que quiero mostrarles. Hoy es Miércoles'),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-          FlatButton(onPressed: () {}, child: Text('Cancelar')),
-          FlatButton(onPressed: () {}, child: Text('Ok')),
-                ],)
-              ],
-            ),
-          );
-                      }
-          
-           Widget _cardTipo2() {
-return Card(
-child: Column(
-  children: [
-    FadeInImage(
-      placeholder: AssetImage('assets/jar-loading.gif'),
-     image:  NetworkImage('https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'),
-     fadeInDuration: Duration(milliseconds: 200),
-     height: 300.0,
-     fit: BoxFit.cover,
-    ),
-    //Image(
-      //image: NetworkImage('https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'),
-    //),
-    Container(
-      padding: EdgeInsets.all(10.0),
-      child: Text('Imagen tomada en 2021')
-      )
-  ],
-),
-);
-            }
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el título de esta tarjeta'),
+            subtitle: Text(
+                'Aquí estamos en la descripción de la tarjeta que quiero que ustedes vean que quiero mostrarles. Hoy es Miércoles'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlatButton(onPressed: () {}, child: Text('Cancelar')),
+              FlatButton(onPressed: () {}, child: Text('Ok')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    
+    final card= Container(
+     // clipBehavior:Clip.antiAlias,
+      child: Column(
+        children: [
+          FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(
+                'https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          //Image(
+          //image: NetworkImage('https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'),
+          //),
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('Imagen tomada en 2021'))
+        ],
+      ),
+    );
+    return Container(
+    
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius:10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0),
+          )
+        ]
+       //color: Colors.red,
+      ),
+        child:ClipRRect(
+          borderRadius: BorderRadius.circular(30.0),
+          child: card
+          ),
+    );
+
+  }
 }
--- Seccion6/leccion6_componentes/lib/src/pages/home_page.dart
+++ Seccion6/leccion6_componentes/lib/src/pages/home_page.dart
@@ -1,71 +1,56 @@
 import 'package:flutter/material.dart';
 
-
 import 'package:leccion6_componentes/src/providers/menu_providers.dart';
-import 'package:leccion6_componentes/src/utils/icono_string_util.dart'; 
-import 'package:leccion6_componentes/src/pages/alert_page.dart';
+import 'package:leccion6_componentes/src/utils/icono_string_util.dart';
+//import 'package:leccion6_componentes/src/pages/alert_page.dart';
 
 class HomePage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text('Componentes'),
-    ),
-    body:_lista(),
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
     );
   }
 
   Widget _lista() {
-  //print(menuProvider.opciones);
-  //menuProvider.cargarData()
-   
-   return FutureBuilder(
-     future: menuProvider.cargarData(),
-     initialData: [],
-builder: ( context, AsyncSnapshot<List<dynamic>> snapshot){
+    //print(menuProvider.opciones);
+    //menuProvider.cargarData()
 
-print(snapshot.data);
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print(snapshot.data);
 
-      return ListView(
-      children: _listaItems(snapshot.data, context),
-       );
-     },
-     
-   );
+        return ListView(
+          children: _listaItems(snapshot.data, context),
+        );
+      },
+    );
+  }
 
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
+    final List<Widget> opciones = [];
 
-    
-        }
-      
-List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
-         
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {
+          Navigator.pushNamed(context, opt['ruta']);
+          //final route= MaterialPageRoute(
+          //builder: (context)=>AlertPage());
+          //Navigator.push(context, route);
+        },
+      );
 
-final List<Widget> opciones= [];
+      opciones..add(widgetTemp)..add(Divider());
+    });
 
-data.forEach((opt){
-
-final widgetTemp=ListTile(
-  title: Text(opt['texto']),
-  leading: getIcon(opt['icon']),
-  trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-  onTap: (){
-
-
-        Navigator.pushNamed(context, opt['ruta']);
-      //final route= MaterialPageRoute(
-        //builder: (context)=>AlertPage());
-      //Navigator.push(context, route);
-  },
-);
-
-opciones..add(widgetTemp)
-..add(Divider());
-});
-
-return opciones;
-
-       }
+    return opciones;
+  }
 }
