1e63799b97835ed3ffb7f333dc21b297f8ef00b9
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-08 00:28:40 -0500

    Seccion 6 Leccion 80 Email y password fields

--- Seccion6/leccion6_componentes/lib/src/pages/input_page.dart
+++ Seccion6/leccion6_componentes/lib/src/pages/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,7 +7,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _nombre;
+  String _nombre="";
+  String _email="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +20,14 @@ class _InputPageState extends State<InputPage> {
         children: [
           _crearInput(),
           Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
           _crearPersona(),
+          Divider(),
+
+
         ],
       ),
     );
@@ -42,21 +51,52 @@ class _InputPageState extends State<InputPage> {
       onChanged: (valor){
         setState((){
           _nombre=valor;
-        });
-        
-        
-
+        });          
       },
     );
   }
 
-
-
-
-
   Widget _crearPersona(){
     return ListTile(
       title: Text('Nombre es : $_nombre'),
+      subtitle: Text('Email: $_email'),
+    );
+  }
+
+  Widget _crearEmail(){
+     return TextField(
+       keyboardType: TextInputType.emailAddress,          
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20.0)
+        ),
+        
+        hintText: 'Email',
+        labelText: 'Email',        
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email)
+      ),
+      onChanged: (valor)=>setState((){
+        _email=valor;
+      })      
+    );
+  }
+
+  Widget _crearPassword(){
+     return TextField(
+       obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20.0)
+        ),        
+        hintText: 'Password',
+        labelText: 'Contraseña',        
+        suffixIcon: Icon(Icons.lock),
+        icon: Icon(Icons.lock)
+      ),
+      onChanged: (valor)=>setState((){
+        _email=valor;
+      })      
     );
   }
 }
