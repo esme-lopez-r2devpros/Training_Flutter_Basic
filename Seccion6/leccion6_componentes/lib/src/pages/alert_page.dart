7c16ade1e8c73211f864e2dffb029e713183613d
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-07 19:42:00 -0500

    Seccion 6 Leccion 76 Widget Alert

--- Seccion6/leccion6_componentes/lib/src/pages/alert_page.dart
+++ Seccion6/leccion6_componentes/lib/src/pages/alert_page.dart
@@ -9,12 +9,54 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(title: Text('Alert Page'),
       
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: ()  => _mostrarAlerta(context),          
+        ),
+      ) ,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.memory),
         onPressed: (){
           Navigator.pop(context);
         }
         ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context){
+
+    showDialog(
+      context: context,
+      barrierDismissible:true,
+      builder: (context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text ('Este es el contenido de la caja de la alerta'),
+              FlutterLogo(size:100.0)
+            ],
+            ),
+            actions: [
+              FlatButton(
+                child:Text('ok'),
+                onPressed: ()=>Navigator.of(context).pop(),
+              ),
+               FlatButton(
+                child:Text('Cancelar'),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+        );
+      }
     );
   }
 }
