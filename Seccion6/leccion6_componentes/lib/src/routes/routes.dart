66f1faef0b952c3a74954230bac98aae0b4621c0
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-07 22:55:09 -0500

    Seccion 6 Leccion 78 Container y AnimatedContainer

--- Seccion6/leccion6_componentes/lib/src/routes/routes.dart
+++ Seccion6/leccion6_componentes/lib/src/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 
 import 'package:leccion6_componentes/src/pages/alert_page.dart';
+import 'package:leccion6_componentes/src/pages/animated_container.dart';
 import 'package:leccion6_componentes/src/pages/avatar_page.dart';
 import 'package:leccion6_componentes/src/pages/card_page.dart';
 import 'package:leccion6_componentes/src/pages/home_page.dart';
@@ -12,6 +13,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
         'alert': (BuildContext context) => AlertPage(),
         'avatar': (BuildContext context) => AvatarPage(),
         'card': (BuildContext context) => CardPage(),
+        'animatedContainer': (BuildContext context) => AnimatedContainerPage()
       };
 }
 
