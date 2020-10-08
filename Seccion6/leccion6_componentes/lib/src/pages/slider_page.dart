1d5f096ba59c653dae50018fe0d6ac6348d38075
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-08 08:51:12 -0500

    Seccion 6 Leccion 85 Checkbox y Switches

--- Seccion6/leccion6_componentes/lib/src/pages/slider_page.dart
+++ Seccion6/leccion6_componentes/lib/src/pages/slider_page.dart
@@ -6,7 +6,8 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
-  double _valorSlider=100.0;
+  double _valorSlider = 100.0;
+  bool _bloquearCheck = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,38 +15,75 @@ class _SliderPageState extends State<SliderPage> {
         title: Text('Sliders'),
       ),
       body: Container(
-        padding: EdgeInsets.only(top:50.0),
-        child:Column(
-              children: [
-                _crearSlider(),
-                Expanded(
-                  child: _crearImagen()
-                  ),
-              ],
-        ), 
-         ),
-        );    
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: [
+            _crearSlider(),
+            _checkBox(),
+            _crearSwitch(),
+            Expanded(child: _crearImagen()),
+          ],
+        ),
+      ),
+    );
   }
 
-  Widget _crearSlider(){
+  Widget _crearSlider() {
     return Slider(
       activeColor: Colors.indigoAccent,
-      label:'Tamaño de la imagen',
-     // divisions: 20,
+      label: 'Tamaño de la imagen',
+      // divisions: 20,
       value: _valorSlider,
-      min:10.0,
-      max:400.0,
-      onChanged: (valor ){
+      min: 10.0,
+      max: 400.0,
+      onChanged: (_bloquearCheck)
+          ? null
+          : (valor) {
+              setState(() {
+                _valorSlider = valor;
+              });
+            },
+    );
+  }
+
+  Widget _checkBox() {
+    /*return Checkbox(
+        value: _bloquearCheck ,
+        onChanged: (valor){
           setState((){
-            _valorSlider=valor;
-          });        
+             _bloquearCheck=valor;
+
+          });
+           
+        }
+    );*/
+    return CheckboxListTile(
+      title: Text('Bloquear slider'),
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
       },
     );
   }
 
-  Widget _crearImagen(){
+  Widget _crearSwitch() {
+    return SwitchListTile(
+      title: Text('Bloquear slider'),
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearImagen() {
     return Image(
-      image: NetworkImage('https://wallpapersite.com/images/wallpapers/beauty-and-the-beast-1440x2560-2017-hd-4k-8k-6368.jpg'),
+      image: NetworkImage(
+          'https://wallpapersite.com/images/wallpapers/beauty-and-the-beast-1440x2560-2017-hd-4k-8k-6368.jpg'),
       width: _valorSlider,
       fit: BoxFit.contain,
     );
