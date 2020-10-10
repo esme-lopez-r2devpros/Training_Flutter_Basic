5093d6329c728a5b52bb2960f759c41cbae47276
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-10 12:46:44 -0500

    Seccion 7 Leccion 110 StreamBuilder

--- Seccion7/seccion7_app_peliculas/lib/src/pages/home_page.dart
+++ Seccion7/seccion7_app_peliculas/lib/src/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
    
   @override
   Widget build(BuildContext context) {
-
+    peliculasProvider.getPopulares();
     return Scaffold(
       appBar: AppBar(
         title: Text('Cartelera'),
@@ -74,12 +74,14 @@ class HomePage extends StatelessWidget {
                padding: EdgeInsets.only(left: 20.0 ),
                child: Text('Populares', style: Theme.of(context).textTheme.subtitle1)),
              SizedBox(height: 10.0),
-             FutureBuilder(
-               future: peliculasProvider.getPopulares(),               
+             StreamBuilder(
+               stream: peliculasProvider.popularesStream,               
                builder: (BuildContext context, AsyncSnapshot<List> snapshot) {  
                  if(snapshot.hasData)
                  {
-                   return MovieHorizontal(peliculas: snapshot.data);
+                   return MovieHorizontal(
+                     peliculas: snapshot.data,
+                      siguientePagina: peliculasProvider.getPopulares );
                  }
                   else{
                      return Center(child: CircularProgressIndicator());                 
--- Seccion7/seccion7_app_peliculas/lib/src/widgets/movie_horizontal.dart
+++ Seccion7/seccion7_app_peliculas/lib/src/widgets/movie_horizontal.dart
@@ -8,21 +8,30 @@ import 'package:seccion7_app_peliculas/src/models/pelicula_model.dart';
 class MovieHorizontal extends StatelessWidget {
 
   final List<Pelicula> peliculas;
+  final Function siguientePagina;
+  MovieHorizontal({@required this.peliculas , @required this.siguientePagina });
 
-  MovieHorizontal({@required this.peliculas});
+  final _pageController = new PageController(
+      initialPage: 1,
+          viewportFraction: 0.3
+    );
 
   @override
   Widget build(BuildContext context) {
-    final _screenSize = MediaQuery.of(context).size;
+    final _screenSize = MediaQuery.of(context).size;    
+
+    _pageController.addListener((){
+      if(_pageController.position.pixels>=_pageController.position.maxScrollExtent -200)
+      {
+        siguientePagina();
+      }
+    });
 
     return Container(
       height: _screenSize.height * 0.2,
       child: PageView(
         pageSnapping: false,
-        controller: PageController(
-          initialPage: 1,
-          viewportFraction: 0.3
-        ),
+        controller: _pageController,
           children: _tarjetas(context),        
       ),
     );
