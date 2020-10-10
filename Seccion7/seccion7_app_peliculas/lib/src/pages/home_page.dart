1c4663f4535920b29a55603e6ccb184a8b86af94
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-10 02:47:09 -0500

    Seccion 7 Leccion 105 Widget Personalizado - Horizontal Page View

--- Seccion7/seccion7_app_peliculas/lib/src/pages/home_page.dart
+++ Seccion7/seccion7_app_peliculas/lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:seccion7_app_peliculas/src/providers/peliculas_provider.dart';
 import 'package:seccion7_app_peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:seccion7_app_peliculas/src/widgets/movie_horizontal.dart';
 
 
 
@@ -67,13 +68,22 @@ class HomePage extends StatelessWidget {
     return Container(
         width: double.infinity,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
            children: [
-             Text('Populares', style: Theme.of(context).textTheme.subtitle1),
+             Container(
+               padding: EdgeInsets.only(left: 20.0 ),
+               child: Text('Populares', style: Theme.of(context).textTheme.subtitle1)),
+             SizedBox(height: 10.0),
              FutureBuilder(
                future: peliculasProvider.getPopulares(),               
-               builder: (BuildContext context, AsyncSnapshot<List> snapshot) {              
-                 snapshot.data?.forEach((p) =>print(p.title));
-               return Container();                 
+               builder: (BuildContext context, AsyncSnapshot<List> snapshot) {  
+                 if(snapshot.hasData)
+                 {
+                   return MovieHorizontal(peliculas: snapshot.data);
+                 }
+                  else{
+                     return Center(child: CircularProgressIndicator());                 
+                  }              
                },
              ),
            ],
