3412bc5a71c95fb97bcef563e900680abbdcfe3d
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-11 01:49:50 -0500

    Seccion 7 Leccion 123 Buscar Peliculas en TheMovieDB

--- Seccion7/seccion7_app_peliculas/lib/src/providers/peliculas_provider.dart
+++ Seccion7/seccion7_app_peliculas/lib/src/providers/peliculas_provider.dart
@@ -77,6 +77,18 @@ Future<List<Actor>> getCast ( String peliId) async{
  return cast.actores;
 }
 
+Future<List<Pelicula>> buscarPelicula(String query) async{
+
+
+      final url= Uri.https(_url,'3/search/movie', {
+          'api_key'   : _apiKey,
+          'language'  : _language,
+          'query'     : query
+
+      });   
+      return await _procesarRespuesta(url);
+}
+
 
 
 }
