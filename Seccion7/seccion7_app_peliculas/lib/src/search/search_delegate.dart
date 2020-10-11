d2a0ef07a0786e359e3b860496ec1d9bd934f120
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-11 01:01:19 -0500

    Seccion 7 Leccion 122 BuildSuggestions y BuildResults

--- Seccion7/seccion7_app_peliculas/lib/src/search/search_delegate.dart
+++ Seccion7/seccion7_app_peliculas/lib/src/search/search_delegate.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate{
+
+String seleccion='';
+
+  final peliculas= [
+    'Spiderman',
+    'Aquaman',
+    'Batman',
+    'Shazam!',
+    'Ironman',
+    'Capitan',
+  ];
+
+  final peliculasRecientes=[
+    'Spiderman',
+    'Capitan America',
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
       // Las acciones de nuestro appbar. por ejemplo un ico para eliminar el texto
@@ -31,13 +48,39 @@ class DataSearch extends SearchDelegate{
     @override
     Widget buildResults(BuildContext context) {
       //Crea los resultados que vamos a mostrar
-      return Container();
+      return Center(
+        child: Container(
+          height: 100.0,
+          width: 100.0,
+          color: Colors.blueAccent,
+          child: Text(seleccion),
+        ),
+      );
     }
   
     @override
     Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando las personas escriben
-    return Container();
+
+    final listaSugerida = (query.isEmpty)
+                        ? peliculasRecientes
+                        : peliculas.where(
+                        (p)=>p.toLowerCase().startsWith(query.toLowerCase())
+                        ).toList();
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, i){
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: (){
+            seleccion=listaSugerida[i];
+            showResults(context);
+          }
+        );
+      },
+    );
   }
 
 }
