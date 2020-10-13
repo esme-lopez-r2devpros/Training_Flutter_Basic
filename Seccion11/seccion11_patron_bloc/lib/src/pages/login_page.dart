8545262987339d500fff8bb4bed70d1f34f99a16
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-12 22:28:16 -0500

    Seccion 11 Leccion 197 Diseño de la caja del login y los campos

--- Seccion11/seccion11_patron_bloc/lib/src/pages/login_page.dart
+++ Seccion11/seccion11_patron_bloc/lib/src/pages/login_page.dart
@@ -7,6 +7,7 @@ class LoginPage extends StatelessWidget {
      body: Stack(
        children: [
          _crearFondo(context),
+         _loginForm(context),
        ],
      ),
     );
@@ -56,6 +57,94 @@ class LoginPage extends StatelessWidget {
         ),
       ],
 
+    );
+  }
+
+  Widget _loginForm(BuildContext context){
+    final size=MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SafeArea(
+            child: Container(
+              height: 180.0,
+            ),
+          ),
+
+          Container(
+            width: size.width*0.85,
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0, 5.0),
+                  spreadRadius: 3.0,
+                ),
+              ]
+            ),
+            child: Column(children: [
+              Text('Ingreso', style: TextStyle(fontSize: 20.0)),
+              SizedBox(height: 60.0),
+              _crearEmail(),
+              SizedBox(height: 30.0),
+              _crearPassword(),
+              SizedBox(height: 30.0),
+              _crearBoton(),
+            ],),
+          ),
+          Text('¿Olvidó la contraseña?'),
+          SizedBox(height: 100.0),
+        ],
+      ),
+
+    );
+  }
+
+  Widget _crearEmail(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child:TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+          hintText: 'ejemplo@correo.com',
+          labelText: 'Correo electrónico',
+        ),
+      ),
+    );    
+}
+
+ Widget _crearPassword(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child:TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock_outline, color: Colors.deepPurple),          
+          labelText: 'Contraseña',
+        ),
+      ),
+    );
+  }
+
+  Widget _crearBoton(){
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+       child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple, 
+      textColor: Colors.white,
+      onPressed: (){},
     );
   }
 }
