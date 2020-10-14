491d4cf3f819a94ec7b1d60c1f482f461d24e09c
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-13 22:02:42 -0500

    Seccion 11 Leccion 201 Conectar los inputs con los Streams

--- Seccion11/seccion11_patron_bloc/lib/main.dart
+++ Seccion11/seccion11_patron_bloc/lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'package:seccion11_patron_bloc/src/bloc/provider.dart';
 
 import 'package:seccion11_patron_bloc/src/pages/home_page.dart';
 import 'package:seccion11_patron_bloc/src/pages/login_page.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner:false,
-      title: 'Material App',
-      initialRoute: 'login',
-      routes: {
-        'login' : (BuildContext context )=> LoginPage(),
-        'home'  : (BuildContext context )=> HomePage(),
-      }      
+    return Provider(
+      child:  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+        ),
+      ),
     );
   }
 }
--- Seccion11/seccion11_patron_bloc/lib/src/pages/login_page.dart
+++ Seccion11/seccion11_patron_bloc/lib/src/pages/login_page.dart
@@ -1,67 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:seccion11_patron_bloc/src/bloc/provider.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: Stack(
-       children: [
-         _crearFondo(context),
-         _loginForm(context),
-       ],
-     ),
+      body: Stack(
+        children: [
+          _crearFondo(context),
+          _loginForm(context),
+        ],
+      ),
     );
   }
 
-  Widget _crearFondo(BuildContext context){
-    final size=MediaQuery.of(context).size;
+  Widget _crearFondo(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     final fondoMorado = Container(
-      height: size.height*0.4,
-      width: double.infinity, 
+      height: size.height * 0.4,
+      width: double.infinity,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: <Color>[
-            Color.fromRGBO(63, 63, 156, 1.0),
-            Color.fromRGBO(90, 70, 78, 1.0),
-          ]
-        ),
+        gradient: LinearGradient(colors: <Color>[
+          Color.fromRGBO(63, 63, 156, 1.0),
+          Color.fromRGBO(90, 70, 78, 1.0),
+        ]),
       ),
     );
 
-    final circulo= Container(
-      width:100.0,
+    final circulo = Container(
+      width: 100.0,
       height: 100.0,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100.0),
-        color:  Color.fromRGBO(255,255,255,0.05),
+        color: Color.fromRGBO(255, 255, 255, 0.05),
       ),
     );
 
     return Stack(
       children: [
         fondoMorado,
-        Positioned(  top:90.0, left: 30.0, child: circulo),
-        Positioned(  top:-40.0, left: -30.0, child: circulo),
-        Positioned(  bottom:-50.0, right: -10.0, child: circulo),
-        Positioned(  bottom:120.0, right: 20.0, child: circulo),
-        Positioned(  bottom:-150.0, left: -20.0, child: circulo),
+        Positioned(top: 90.0, left: 30.0, child: circulo),
+        Positioned(top: -40.0, left: -30.0, child: circulo),
+        Positioned(bottom: -50.0, right: -10.0, child: circulo),
+        Positioned(bottom: 120.0, right: 20.0, child: circulo),
+        Positioned(bottom: -150.0, left: -20.0, child: circulo),
         Container(
           padding: EdgeInsets.only(top: 50.0),
           child: Column(
             children: [
               Icon(Icons.person_pin_circle, color: Colors.white, size: 100),
               SizedBox(height: 10.0, width: double.infinity),
-              Text('Esmeralda Lopez', style: TextStyle(color: Colors.white, fontSize: 25.0 )),
+              Text('Esmeralda Lopez',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0)),
             ],
           ),
         ),
       ],
-
     );
   }
 
-  Widget _loginForm(BuildContext context){
-    final size=MediaQuery.of(context).size;
+  Widget _loginForm(BuildContext context) {
+    final bloc = Provider.of(context);
+    final size = MediaQuery.of(context).size;
+
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -70,81 +71,94 @@ class LoginPage extends StatelessWidget {
               height: 180.0,
             ),
           ),
-
           Container(
-            width: size.width*0.85,
+            width: size.width * 0.85,
             margin: EdgeInsets.symmetric(vertical: 30.0),
             padding: EdgeInsets.symmetric(vertical: 50.0),
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5.0),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 3.0,
-                  offset: Offset(0.0, 5.0),
-                  spreadRadius: 3.0,
-                ),
-              ]
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 3.0,
+                    offset: Offset(0.0, 5.0),
+                    spreadRadius: 3.0,
+                  ),
+                ]),
+            child: Column(
+              children: [
+                Text('Ingreso', style: TextStyle(fontSize: 20.0)),
+                SizedBox(height: 60.0),
+                _crearEmail(bloc),
+                SizedBox(height: 30.0),
+                _crearPassword(bloc),
+                SizedBox(height: 30.0),
+                _crearBoton(),
+              ],
             ),
-            child: Column(children: [
-              Text('Ingreso', style: TextStyle(fontSize: 20.0)),
-              SizedBox(height: 60.0),
-              _crearEmail(),
-              SizedBox(height: 30.0),
-              _crearPassword(),
-              SizedBox(height: 30.0),
-              _crearBoton(),
-            ],),
           ),
           Text('¿Olvidó la contraseña?'),
           SizedBox(height: 100.0),
         ],
       ),
-
     );
   }
 
-  Widget _crearEmail(){
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child:TextField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-          icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
-          hintText: 'ejemplo@correo.com',
-          labelText: 'Correo electrónico',
-        ),
-      ),
-    );    
-}
-
- Widget _crearPassword(){
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child:TextField(
-        obscureText: true,
-        decoration: InputDecoration(
-          icon: Icon(Icons.lock_outline, color: Colors.deepPurple),          
-          labelText: 'Contraseña',
-        ),
-      ),
+  Widget _crearEmail(LoginBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.emailStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+              hintText: 'ejemplo@correo.com',
+              labelText: 'Correo electrónico',
+              counterText: snapshot.data,
+            ),
+            onChanged: bloc.changeEmail,
+          ),
+        );
+      },
     );
   }
 
-  Widget _crearBoton(){
+  Widget _crearPassword(LoginBloc bloc) {
+   return  StreamBuilder(
+      stream: bloc.passwordStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
+              labelText: 'Contraseña',
+              counterText:snapshot.data
+            ),
+            onChanged: bloc.changePassword,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _crearBoton() {
     return RaisedButton(
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-       child: Text('Ingresar'),
+        child: Text('Ingresar'),
       ),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(5.0),
       ),
       elevation: 0.0,
-      color: Colors.deepPurple, 
+      color: Colors.deepPurple,
       textColor: Colors.white,
-      onPressed: (){},
+      onPressed: () {},
     );
   }
 }
