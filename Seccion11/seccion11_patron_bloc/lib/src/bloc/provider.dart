bab81f8cdb3f70909354930ed5cf77cb356aebab
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-14 00:25:57 -0500

    Seccion 11 Leccion 206 Mantener la data de los Streams después de un hot reload

--- Seccion11/seccion11_patron_bloc/lib/src/bloc/provider.dart
+++ Seccion11/seccion11_patron_bloc/lib/src/bloc/provider.dart
@@ -5,11 +5,21 @@ export 'package:seccion11_patron_bloc/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget{
 
-  final loginBloc = LoginBloc();
+  static Provider _instancia;
+  factory Provider({Key key, Widget child}){
+      if(_instancia==null){
+        _instancia=new Provider._internal(key: key, child: child);
+      }
+      return _instancia;
+  }
 
-  Provider({Key key, Widget child})
+   Provider._internal({Key key, Widget child})
   : super(key: key, child: child);
 
+
+  final loginBloc = LoginBloc();
+
+ 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
 
