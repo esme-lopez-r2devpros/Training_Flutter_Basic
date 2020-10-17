acb6417ec1b0e57aa1c7e3af98347f58a7312d81
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-17 17:39:40 -0500

    Seccion 12 CRUD Leccion 230 Pruebas de carga de fotografías y actualizar registros en Firebase

--- Seccion11/seccion11_patron_bloc/lib/src/providers/productos_provider.dart
+++ Seccion11/seccion11_patron_bloc/lib/src/providers/productos_provider.dart
@@ -3,8 +3,10 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:mime_type/mime_type.dart';
 import 'package:seccion11_patron_bloc/src/models/producto_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 
 class ProductosProvider {
 
@@ -64,6 +66,31 @@ class ProductosProvider {
   Future<String> subirImagen(File imagen) async{
 
       final url= Uri.parse('https://res.cloudinary.com/dqoq675qk/image/upload/v1602968342/m3f3mrm5ir4ko9wcct0j.jpg');
-      
+      final mimeType= mime(imagen.path).split('/'); //Image/jpeg
+
+      final imageUploadRequest = http.MultipartRequest(
+        'POST',
+        url
+      );
+
+      final file =await http.MultipartFile.fromPath(
+        'file',
+        imagen.path,
+        contentType: MediaType(mimeType[0], mimeType[1])
+      );
+
+      imageUploadRequest.files.add(file);
+
+      final streamResponse= await imageUploadRequest.send();
+      final resp= await http.Response.fromStream(streamResponse);
+      if(resp.statusCode != 200 && resp.statusCode!= 201){
+        print('Algo salió mal');
+        print(resp.body);
+        return null;
+      }
+
+      final respData= json.decode(resp.body);
+      print(respData);
+      return respData['secure_url'];
   }
 }
