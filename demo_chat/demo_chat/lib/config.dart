71c841f56131a4d010beb3629547b8d6823e5d46
chdsz123 <christian.salazar@r2devrpos.com>
2020-11-04 00:18:23 -0600

    adding Christian User

--- demo_chat/demo_chat/lib/config.dart
+++ demo_chat/demo_chat/lib/config.dart
@@ -1,9 +1,9 @@
 class Config{
-  static final String apiKey = 'gzuh3rrrsr7q';
+  static final String apiKey = 'j4tcpcp2vsg7';
 
 static final String tokenEmily =
-      'v7gsgr8ae7xzu3qwugwfxvk4v492fxghpmsvqmwzmakx8p5qczva3dd8b22y4w6r';
-  static final String idEmily = 'falling-mountain-7';
+      'zjjxykyhakhke5kvpvas9sbkrhzqwfxdjd2bm9p9ux732fwayjdjbhbebe72vx5s';
+  static final String idEmily = 'chdsz123';
 
   static final String tokenPeter =
       'v7gsgr8ae7xzu3qwugwfxvk4v492fxghpmsvqmwzmakx8p5qczva3dd8b22y4w6r';
